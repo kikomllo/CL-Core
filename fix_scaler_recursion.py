c95c3b0438c458124f5e84405508ddb0bcb6589d
kikomllo <a84085@ualg.pt>
2026-08-30 16:47:59 +0100

    Deleted debug files from the project folder

--- fix_scaler_recursion.py
+++
@@ -1,9 +0,0 @@
-with open("src/clUIScaler.py", "r") as f:
-    code = f.read()
-
-code = code.replace("screens = self.get_stable_screens()\\n        reg_file", "screens = QApplication.screens()\\n        reg_file")
-
-with open("src/clUIScaler.py", "w") as f:
-    f.write(code)
-
-print("done")
